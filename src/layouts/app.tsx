800fcb2dc4d260ffb1b9aec8e28b7144ba22eb81
wslyvh <wslyvh@users.noreply.github.com>
2020-10-13 09:41:10 +0200

    cra

---
+++ src/layouts/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Tokenlog</h1>
+      </header>
+      <div className="container">
+        <p>Token weighted backlogs</p>
+      </div>
+    </div>
+  );
+}
