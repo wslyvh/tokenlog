a925aa02a4e64f923ab35706f64c57ff8483e40c
wslyvh <wslyvh@users.noreply.github.com>
2021-01-15 09:14:34 +0100

    TEC IHT

--- src/components/FeaturedRepositories/FeaturedRepositories.tsx
+++ src/components/FeaturedRepositories/FeaturedRepositories.tsx
@@ -51,6 +51,12 @@ const projects = [
     imageUrl: 'https://tokenlog.xyz/icon.png',
     link: '/wslyvh/tokenlog',
   },
+  {
+    title: 'TECommons',
+    description: 'Coordination for all the work across the Token Engineering Commons (TEC)',
+    imageUrl: 'https://avatars1.githubusercontent.com/u/72481541?s=200&v=4',
+    link: '/TECommons/coordination',
+  }
 ];
 
 export function FeaturedRepositories() {
