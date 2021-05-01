f017cfbf33d70db9a49df97a4b9e11e2f6ad3c42
wslyvh <wslyvh@users.noreply.github.com>
2021-05-01 13:23:49 +0200

    stats link

--- src/pages/Repository.tsx
+++ src/pages/Repository.tsx
@@ -4,13 +4,21 @@ import { RepositoryInfo } from 'components/RepositoryInfo';
 import { TokenInfo } from 'components/TokenInfo';
 import { RepoContextProvider } from 'context/RepoContextProvider';
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Repository() {
+  const { org, repo } = useParams();
+  
   return (
     <>
       <div>
         <RepoContextProvider>
           <NetworkAlert />
+
+          <div className="float-right">
+            <Link to={`stats`}>view stats &raquo;</Link>
+          </div>
+          <br />
 
           <div className="card-deck">
             <RepositoryInfo />
